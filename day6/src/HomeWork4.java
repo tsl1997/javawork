b6b6e3c37849b524faa828aeb0a72969c6bdb54b
xs <btcabc@163.com>
2022-03-12 19:44:35 +0800

    day6扩展作业，打印菱形，语法更加健壮，适用于任何输入的奇数

--- day6/src/HomeWork4.java
+++ day6/src/HomeWork4.java
@@ -5,15 +5,23 @@ public class HomeWork4 {
 		//菱形
 		//1:9行，上下对称，故-4到+4
 		Scanner input = new Scanner(System.in);
-		System.out.print("请输入需要打印的菱形行数(奇数）：");
-		int num = input.nextInt();
-		for (int i=1;i<=num/2;i++){
+		int num;
+		do {
+			System.out.print("请输入需要打印的菱形行数(奇数）：");
+			num = input.nextInt();
+			if (num%2!=0){
+				break;//当输入数据合法时跳出循环
+			}
+			System.out.println("输入数据不合法！");
+		}while (true);
+
+		for (int i=-num/2;i<=num/2;i++){
 			//2:打印空格
 			for (int j=1;j<=Math.abs(i);j++){
 				System.out.print("\t");
 			}
 			//3:打印星星
-			for (int k=1;k<=(5-Math.abs(i))*2-1;k++){
+			for (int k=1;k<=num-Math.abs(i)*2;k++){
 				System.out.print("*\t");
 			}
 			System.out.println();//换行
