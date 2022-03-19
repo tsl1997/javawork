6014dfee3f85d879c664101e51484ebfcfd9e2c1
xs <btcabc@163.com>
2022-03-19 16:06:14 +0800

    面向对象第二课：课堂小作业,计算器

---
+++ day11/src/Counter.java
@@ -0,0 +1,8 @@
+public class Counter {
+	/**
+	 * @param a 第一个变量
+	 * @param b 第二个变量*/
+	public void add(int a,int b){
+		System.out.println(a+"+"+b+"="+(a+b));
+	}
+}
---
+++ day11/src/CounterTest.java
@@ -0,0 +1,7 @@
+import java.util.Calendar;
+
+public class CounterTest {
+	public static void main(String[] args) {
+		new Counter().add(5,6);//调用方法
+	}
+}
