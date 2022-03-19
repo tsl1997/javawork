8b9408e9388898dd9a25550b30ffd202ee3855ac
xs <btcabc@163.com>
2022-03-19 21:14:06 +0800

    面向对象第二课：课后作业，榨汁机类

---
+++ day11/src/Juicer.java
@@ -0,0 +1,8 @@
+/**这是一个榨汁机类*/
+public class Juicer {
+	/**把水果转化为果汁
+	 * @param a 水果*/
+	public void fruit(String a){
+		System.out.println(a+"汁");
+	}
+}
---
+++ day11/src/TestJuicer.java
@@ -0,0 +1,5 @@
+public class TestJuicer {
+	public static void main(String[] args) {
+		new Juicer().fruit("苹果");//传入参数，调用方法
+	}
+}
