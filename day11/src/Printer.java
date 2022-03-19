7ac602ed348df1d0d1c98635ab35f36ab7484e35
xs <btcabc@163.com>
2022-03-19 14:59:56 +0800

    面向对象第二课：课堂小作业

---
+++ day11/src/Printer.java
@@ -0,0 +1,11 @@
+public class Printer {
+	public void print(){
+		for (int i=0;i<3;i++){
+			for (int j=0;j<3;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ day11/src/PrinterTest.java
@@ -0,0 +1,6 @@
+public class PrinterTest {
+	public static void main(String[] args) {
+		Printer printer = new Printer();//创建对象
+		printer.print();//调用方法
+	}
+}
