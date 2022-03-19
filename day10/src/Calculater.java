c84991622870be6fb065b0baeacdc264d5b69919
xs <btcabc@163.com>
2022-03-19 09:04:20 +0800

    面向对象第1课：课后作业：计算器

---
+++ day10/src/Calculater.java
@@ -0,0 +1,12 @@
+public class Calculater {
+	public int x;
+	public int y;
+
+	public void add(){
+		System.out.println("x+y="+(x+y));
+	}
+
+	public void decreasev(){
+		System.out.println("x-y="+(x-y));
+	}
+}
---
+++ day10/src/TestCalculater.java
@@ -0,0 +1,10 @@
+public class TestCalculater {
+	public static void main(String[] args) {
+		Calculater calculater = new Calculater();
+		calculater.x=100;
+		calculater.y=50;
+
+		calculater.add();
+		calculater.decreasev();
+	}
+}
