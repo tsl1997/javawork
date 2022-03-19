f1f1774ab4f83c87d4b8bb9e57b1f0fc5bde64c3
xs <btcabc@163.com>
2022-03-19 17:01:30 +0800

    面向对象第二课：课堂小作业,计算器，计算多个整数

--- day11/src/Counter.java
+++ day11/src/Counter.java
@@ -1,8 +1,18 @@
 public class Counter {
-	/**
+	/**加法
 	 * @param a 第一个变量
 	 * @param b 第二个变量*/
-	public void add(int a,int b){
-		System.out.println(a+"+"+b+"="+(a+b));
+	public int add(int a,int b,int c){
+		int sum=a+b+c;
+		return sum;
+	}
+	/**多个整数求和，传入数组
+	 * @param a 第一个数组*/
+	public int adds(int[] a){
+		int sum=0;
+		for (int i=0;i<a.length;i++){
+			sum+=a[i];
+		}
+		return sum;
 	}
 }
--- day11/src/CounterTest.java
+++ day11/src/CounterTest.java
@@ -1,7 +1,13 @@
+import java.sql.SQLOutput;
 import java.util.Calendar;
 
 public class CounterTest {
 	public static void main(String[] args) {
-		new Counter().add(5,6);//调用方法
+		int sum=new Counter().add(5,6,9);//调用方法
+		System.out.println("和是："+sum);
+
+		int[] nums={12,23,34,43,45,67};//创建数组
+		int sum2=new Counter().adds(nums);//传入数组参数
+		System.out.println("多个整数求和是："+sum2);
 	}
 }
