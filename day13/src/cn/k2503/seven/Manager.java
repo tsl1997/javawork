46c81d592dcabafd32ee56f4995a522e9f871842
xs <btcabc@163.com>
2022-03-23 16:13:11 +0800

    课后第7题：经理类编写完成

---
+++ day13/src/cn/k2503/seven/Manager.java
@@ -0,0 +1,12 @@
+package cn.k2503.seven;
+/**这是经理类*/
+public class Manager extends Staff{
+		/*奖金
+	 行为：工作work:分配项目任务(用输出语句模拟)*/
+	/**奖金*/
+	private int bonus;
+	/**分配项目任务*/
+	public void work(){
+		System.out.println("分配项目任务");
+	}
+}
