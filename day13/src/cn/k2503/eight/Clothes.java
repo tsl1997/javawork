2ef6134d57bb5ef8f056c1accd3a193cc911144b
xs <btcabc@163.com>
2022-03-23 17:06:45 +0800

    课后第8题：建立女朋友，衣服类

---
+++ day13/src/cn/k2503/eight/Clothes.java
@@ -0,0 +1,23 @@
+package cn.k2503.eight;
+/**衣服类*/
+public class Clothes {
+	//属性:品牌,颜色
+	private String brand;//品牌
+	private String color;//颜色
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
---
+++ day13/src/cn/k2503/eight/GirlFriend.java
@@ -0,0 +1,10 @@
+package cn.k2503.eight;
+
+public class GirlFriend extends Human{
+	private String face;//脸型
+	/**构造函数*/
+	public GirlFriend(String name, double height,String face) {
+		super(name, height);
+	}
+
+}
