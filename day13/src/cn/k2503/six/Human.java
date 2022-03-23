e1096c460c42cdb90da43314128a7df81b11a2d5
xs <btcabc@163.com>
2022-03-23 15:48:21 +0800

    课后第6题：父类子类编写成功

---
+++ day13/src/cn/k2503/six/Human.java
@@ -0,0 +1,26 @@
+package cn.k2503.six;
+
+public class Human {
+	private String name;//姓名
+	private int age;//年龄
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if (age <= 0 || age>100){
+			this.age = 1;
+			return; //人类年龄不能为负数
+		}
+		this.age = age;
+	}
+}
---
+++ day13/src/cn/k2503/six/Student.java
@@ -0,0 +1,52 @@
+package cn.k2503.six;
+
+public class Student extends Human{
+	/**语文成绩*/
+	private int chinese;
+	/**数学成绩*/
+	private int math;
+	/**英语成绩*/
+	private int english;
+
+	public int getChinese() {
+		return chinese;
+	}
+
+	public void setChinese(int chinese) {
+		if (chinese <= 0){
+			chinese = 1;
+			return;//成绩不能为负数
+		}
+		this.chinese = chinese;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		if (math <= 0){
+			math = 1;
+			return;//成绩不能为负数
+		}
+		this.math = math;
+	}
+
+	public int getEnglish() {
+		return english;
+	}
+
+	public void setEnglish(int english) {
+		if (english <= 0){
+			english = 1;
+			return;//成绩不能为负数
+		}
+		this.english = english;
+	}
+
+	public String  getSum(){
+		int sum=chinese+math+english;//计算总分
+		return getName()+"语文："+getChinese()+"\t数学："+getMath()+"\t英语："
+				+getEnglish()+"\t总分:"+sum;
+	}
+}
