8727ec46dc4de350d246c0f7af6e815de484e468
xs <btcabc@163.com>
2022-03-26 14:45:16 +0800

    课后作业2 toString不必单独去写

--- day16/src/cn/k2502/homeWork2/Tester.java
+++ day16/src/cn/k2502/homeWork2/Tester.java
@@ -6,9 +6,9 @@ public class Tester {
 		Pet[] pets = new Pet[2];//创建父类对象
 		pets[0] = new Dog("旺财", 56, 88);//创建狗狗对象
 		pets[1] = new Penguin("腾讯", 99, 1);//创建企鹅对象
-		System.out.println(pets[0].toString());//打印狗狗的自白
+		System.out.println(pets[0]);//打印狗狗的自白
 		System.out.println(m.treat(pets[0]));//健康判断
-		System.out.println(pets[1].toString());//打印企鹅的自白
+		System.out.println(pets[1]);//打印企鹅的自白
 		System.out.println(m.treat(pets[1]));//健康判断
 
 	}
