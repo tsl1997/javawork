5a13eb4060e966b8d92a72304c3e210ffef46742
xs <btcabc@163.com>
2022-03-26 14:49:15 +0800

    课后作业3 莫名其妙跑起来了

---
+++ day16/src/cn/k2502/homeWork3/Master.java
@@ -0,0 +1,12 @@
+package cn.k2502.homeWork3;
+
+public class Master {
+		/**看病技能*/
+		public String treat(Pet pet){
+			if (pet.getHP()<60){
+				return "打针，吃药！";
+			}else{
+				return "我很健康！";
+			}
+		}
+}
