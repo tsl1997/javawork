f36de0c3ce003ab37b06f60954be92705876b0f3
xs <btcabc@163.com>
2022-03-27 09:49:04 +0800

    汽车租聘系统 狗屎一样的代码V1.1(不准备再改了，准备重做）

--- day16/src/cn/k2502/Car/Tester.java
+++ day16/src/cn/k2502/Car/Tester.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**这是一个测试类*/
 public class Tester {
 	/**打印方法*/
-	public static void daying(){
+	public static void daYing(){
 		for (int i = 0; i <15;i++) {
 			System.out.print("*");
 		}
@@ -18,9 +18,9 @@ public class Tester {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);//设置监听器
 		//打印进入页面
-		daying();//打印星星
+		daYing();//打印星星
 		System.out.print("欢迎光临腾飞汽车租聘公司");
-		daying();
+		daYing();
 		System.out.println("\n1、轿车\t 2、客车");
 		System.out.print("请选择你要租聘的汽车类型(1或者2)：");
 		int type = input.nextInt();//获取汽车类型1轿车2客车
@@ -33,9 +33,9 @@ public class Tester {
 				if (t == 1) {
 					System.out.print("请输入你要租聘的天数：");
 					int days = input.nextInt();//获取租聘天数
-					Car car1 = new Car1();//创建对象
-					System.out.println("分配给你的汽车牌号是："+((Car1)car1).carNumber);//获取汽车牌号属性
-					double money=car1.calRent(days,((Car1)car1).money);//调试时使用，查看金额计算是否正确
+					Car1 car1 = new Car1();//创建对象
+					System.out.println("分配给你的汽车牌号是："+ car1.carNumber);//获取汽车牌号属性
+					double money=car1.calRent(days, car1.money);//调试时使用，查看金额计算是否正确
 					System.out.println("你需要支付的租聘费用是："+money);//获取租金
 				}else if(t==2){
 					System.out.print("请输入你要租聘的天数：");
@@ -46,7 +46,6 @@ public class Tester {
 					System.out.println("你需要支付的租聘费用是："+money);//获取租金
 				}else {
 					bug();
-					return;
 				}
 			}else if (brand==2){
 				System.out.print("请选择你要租聘的汽车型号：1、林荫大道\t2、GL8 ：");
@@ -67,11 +66,9 @@ public class Tester {
 					System.out.println("你需要支付的租聘费用是："+money);//获取租金
 				}else {
 					bug();
-					return;
 				}
 			}else{
 				bug();
-				return;
 			}
 
 		}else if(type==2){
@@ -96,7 +93,6 @@ public class Tester {
 					System.out.println("你需要支付的租聘费用是："+money);//获取租金
 				}else {
 					bug();
-					return;
 				}
 			}else if (brand==2){
 				System.out.print("请选择你要租聘的汽车座位数：1、16座\t2、34座 ：");
@@ -117,11 +113,9 @@ public class Tester {
 					System.out.println("你需要支付的租聘费用是："+money);//获取租金
 				}else {
 					bug();
-					return;
 				}
 			}else{
 				bug();
-				return;
 			}
 		}else {
 			bug();
