022ccc944a7703bf967bf677ebd954da494341cd
xs <btcabc@163.com>
2022-03-28 22:45:13 +0800

    demo 默写记忆，修正记忆

--- day17/src/cn/Demo.java
+++ day17/src/cn/Demo.java
@@ -5,25 +5,29 @@ public class Demo {
 	* java基础：
 	* day1:环境安装JDK helloWorld
 	* day2:数据类型：
-	* 基本数据类型8种：byte short int long float double char Boolean
+	* 基本数据类型8种：byte short int long float double char boolean
 	* 引用类型：string 数组 对象 类 接口
+	* (连接符 \t \n 变量 常量final)
 	* day3:运算符
+	* Scanner类
 	* 算术运算符,关系运算符，逻辑运算符，赋值运算符
-	* day4：数组
-	* day5:二维数组，基本算法(冒泡，选择，二分法)
-	* day6:综合整理：吃货联盟
+	* 一元运算符(++ --)三元运算符(?:) 数据类型转换
+	* day4:流程控制-选择结构 if else switch case
+	* day5:流程控制-选择结构 while循环 return string.equals
+	* day6:流程控制-循环结构 for循环 do-while循环 break continue
+	* day7:数组(定义,元素,遍历,应用)
+	* day8:数组，基本算法(排序：冒泡，选择，查找：二分法) Arrays类 增强for
+	* day9:二维数组 null异常 吃货联盟
 	*
 	* java面向对象基础：
-	* day7:包,类,对象 面向过程和面向对象
-	* day8:类的组成：属性和方法
-	* day9:封装 get set
-	* day10:继承 extends 子类和父类 三大特性
-	* day11:构造方法 this super
-	* day12:重载和重写
-	* day13:多态 向上转型和向下转型 instanceof
-	* day14:Object类 重写toString equals
-	* day15:static静态 汽车租赁系统
-	*
+	* day10:类,对象 面向过程和面向对象 对象数组
+	* day11:类的组成：属性和方法 形参和实参 重载
+	* day12:按值传递和按引用类型传递 全局和局部变量
+	* day13:包 构造方法 this 封装(私有化private getter setter)
+	* day14: 继承 extends 三大特性 super 重写
+	* day15:abstract抽象 final修饰 Object类(toString equals)
+	* day16:多态 向上转型和向下转型 instanceof 父类做形参,返回值
+	* day17:static静态 汽车租赁系统
 	*
 	* */
 }
