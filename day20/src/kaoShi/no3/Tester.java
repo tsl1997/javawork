e5552bf214155fd2cfc357fdce57cecff11c93c7
xs <btcabc@163.com>
2022-04-06 12:02:33 +0800

    又考试 第3题红球去重

--- day20/src/kaoShi/no3/Tester.java
+++ day20/src/kaoShi/no3/Tester.java
@@ -8,8 +8,18 @@ public class Tester {
 		int blue = r.nextInt(16) + 1;//保存蓝色的球
 		int[] red =new int[6];//创建红球数组
 		for(int i=0;i<red.length;i++){
-			int temp = r.nextInt(33) + 1;//临时红球
-			red[i] =temp;
+			boolean is;//流程控制
+			int temp;//临时红球
+			do {
+				is=false;//每轮开始重置
+				temp = r.nextInt(33) + 1;//生成红球
+				for (int j = 0; j < i; j++) {
+					if (temp == red[j]) {
+						is=true;//如果相同，重复执行
+					}
+				}
+			}while(is);
+			red[i] =temp;//取出不重复的红球
 		}
 		System.out.println("蓝球：\t\t\t红球：");
 		System.out.print(blue+"\t\t\t");
